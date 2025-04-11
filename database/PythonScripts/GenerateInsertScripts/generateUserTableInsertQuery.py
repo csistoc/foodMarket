c1e9ec10394f950b735a1e45c45078b0a3d181fe
iscatalin <catalin.stefan.istoc@gmail.com>
2025-04-11 12:02:38 +0300

    Added python config files and updated python scripts

--- database/PythonScripts/GenerateInsertScripts/generateUserTableInsertQuery.py
+++ database/PythonScripts/GenerateInsertScripts/generateUserTableInsertQuery.py
@@ -2,7 +2,7 @@ import random
 import sys
 
 from database.PythonScripts.auxScripts import get_string_list_by_words, generate_strong_random_string, \
-    generate_random_date
+    generate_random_date, generate_fake_phone_number, escape_sql
 
 # first argument is the username read file
 # second argument is the country read file
@@ -12,18 +12,24 @@ from database.PythonScripts.auxScripts import get_string_list_by_words, generate
 
 name_list = get_string_list_by_words(sys.argv[1])
 country_list = get_string_list_by_words(sys.argv[2])
-filedata_write = ""
+insert = ""
 
 for i in range(int(sys.argv[5])):
     rnd_name = random.sample(name_list, 2)  # Pick 2 unique names
-    username = rnd_name[0] + rnd_name[1]
-    password = generate_strong_random_string(12)
-    email = rnd_name[0] + "." + rnd_name[1] + "@email.com"
-    first_name = rnd_name[0]
-    last_name = rnd_name[1]
-    address = random.choice(country_list)
-    date_of_birth = generate_random_date("01-01-1950", "01-01-2000")
-    filedata_write += "INSERT INTO " + sys.argv[4] + " VALUES (DEFAULT, '" + username + "', '" + password + "', '" + email + "', '" + first_name + "', '" + last_name + "', '" + address + "', " + str(date_of_birth) + ", 0, 0);\n"
+    username = escape_sql(rnd_name[0] + rnd_name[1])
+    password = escape_sql(generate_strong_random_string(12))
+    email = escape_sql(rnd_name[0] + "." + rnd_name[1] + "@email.com")
+    first_name = escape_sql(rnd_name[0])
+    last_name = escape_sql(rnd_name[1])
+    address = escape_sql(random.choice(country_list))
+    phone_number = escape_sql(generate_fake_phone_number())
+    date_of_birth = generate_random_date("01-01-1950", "01-01-2000").strftime('%Y-%m-%d')
+    #filedata_write += "INSERT INTO " + sys.argv[4] + " VALUES (DEFAULT, '" + username + "', '" + password + "', '" + email + "', '" + first_name + "', '" + last_name + "', '" + address + "', " + str(date_of_birth) + ", 0, 0);\n"
+    insert += (
+        f"INSERT INTO {sys.argv[4]} VALUES (DEFAULT, '{username}', '{password}', "
+        f"'{email}', '{first_name}', '{last_name}', '{address}', '{str(phone_number)}', "
+        f"'{date_of_birth}', FALSE, FALSE);\n"
+    )
 
 with (open(sys.argv[3], "w") as fw):
-    fw.write(filedata_write)
+    fw.write(insert)
--- database/PythonScripts/auxScripts.py
+++ database/PythonScripts/auxScripts.py
@@ -20,7 +20,7 @@ def get_string_list_by_words(file_path):
     return words
 
 def generate_strong_random_string(length = 12):
-    chars = string.ascii_letters + string.digits + string.punctuation
+    chars = string.ascii_letters + string.digits # + string.punctuation
     return ''.join(random.choice(chars) for _ in range(length))
 
 def generate_random_date(start_date, end_date):
@@ -33,3 +33,32 @@ def generate_random_date(start_date, end_date):
     delta = end_date - start_date
     random_days = random.randint(0, delta.days)
     return start_date + timedelta(days = random_days)
+
+def generate_fake_phone_number():
+    country_code = "+1"
+    area_code = "555"  # Reserved for test numbers
+    first_three = random.randint(100, 999)
+    last_four = random.randint(1000, 9999)
+    return f"{country_code}-{area_code}-{first_three}-{last_four}"
+
+# escapes all SQL special characters to prevent SQL injection or syntax errors
+def escape_sql(value):
+    value = value.replace("'", "''") # Escape single quotes by doubling them
+    value = value.replace("\\", "\\\\") # Escape backslashes by doubling them
+    value = value.replace('"', '\\\\"')  # Escape double quotes
+    value = value.replace(";", "\\\\;")  # Escape semicolon
+    value = value.replace("--", "\\\\--")  # Escape double dashes (used for comments in SQL)
+    value = value.replace("/*", "\\\\/*")  # Escape block comment start
+    value = value.replace("*/", "\\\\*/")  # Escape block comment end
+    value = value.replace("/*", "\\\\/*")  # Escape start of block comment
+    value = value.replace("*/", "\\\\*/")  # Escape end of block comment
+    value = value.replace("\n", "\\\\n")  # Escape newlines
+    value = value.replace("\r", "\\\\r")  # Escape carriage returns
+    value = value.replace("\t", "\\\\t")  # Escape tabs
+    value = value.replace("|", "\\\\|") # Escape pipe character
+    value = value.replace("_", "\\\\_") # Escape underline
+    value = value.replace(">", "\\\\>").replace("<", "\\\\<") # Escape greater-than and less-than signs
+    value = value.replace("&", "\\\\&") # Escape ampersands (&)
+    value = value.replace("!", "\\\\!") # Escape exclamation marks (!)
+    value = value.replace("@", "\\\\@") # Escape the @ symbol
+    return str(value)
